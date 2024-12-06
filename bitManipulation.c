f495f904581fce0b7bad685964c707eb54ec41dd
Yoga <yogat3721@gmail.com>
2024-12-06 11:17:00 +0530

    added the setbit and clear bit logic

--- bitManipulation.c
+++ bitManipulation.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+void setBit(int x, int pos)
+{
+    x = (x|(1<<pos));
+    printf("%d ", x); 
+}
+
+void clearBit(int x, int pos)
+{
+    x = (x & (~(1<<pos)));
+    printf("%d ", x);
+}
+
 void powerTwo(int x)
 {
     printf("%d ", (x &&(!(x&(x-1)))));
@@ -8,4 +20,6 @@ void powerTwo(int x)
 int main()
 {
     powerTwo(16);
+    setBit(1, 1);
+    clearBit(3, 1);
 }
