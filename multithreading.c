bf35dbeefcd0a79ca2ab92a09645cbe44611cd8e
Yoga <yogat3721@gmail.com>
2024-12-09 11:23:29 +0530

    Added semaphore logic

--- multithreading.c
+++ multithreading.c
@@ -9,33 +9,64 @@ Welcome to GDB Online.
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <semaphore.h>
+sem_t mutex;
+int turn = 0;
 
-void* myThreadFun(void *argv)
+void* printFizz(void *argv)
 {
+    sem_wait(&mutex);
     for(int i = 0; i < 50 ; i++)
     {
-        printf("Fizz\n"); 
+        while(1)
+        {
+           if(turn == 0)
+           {
+            printf("Fizz\n");
+            turn = 1;
+            sem_post(&mutex);
+            break;
+           }
+        }
+       usleep(1);
     }
-
+    sem_post(&mutex);
     return NULL;
 }
 
-void* myThreadFun2(void *argv)
+void* printBuzz(void *argv)
 {
+    sem_wait(&mutex);
     for(int i = 0; i < 50 ; i++)
     {
-        printf("Buzz\n"); 
+        while(1)
+        {
+           if(turn == 1)
+           {
+            printf("Buzz\n");
+            turn = 0;
+            sem_post(&mutex);
+            break;
+           }
+            sem_post(&mutex);
+        }
+        usleep(1);
+
     }
+
     return NULL;
 }
 
 int main()
 {
-    pthread_t thread_id;
+    sem_init(&mutex, 0, 1);
+    pthread_t fizzthread, buzzthread;
     printf("Before thread\n");
-    pthread_create(&thread_id, NULL, myThreadFun, NULL);
-    pthread_create(&thread_id, NULL, myThreadFun2, NULL);
-    pthread_join(thread_id, NULL);
+    pthread_create(&fizzthread, NULL, printFizz, NULL);
+    pthread_create(&buzzthread, NULL, printBuzz, NULL);
+    pthread_join(fizzthread, NULL);
+    pthread_join(buzzthread, NULL);
     printf("After thread");
+    sem_destroy(&mutex);
     return 0;
 }
